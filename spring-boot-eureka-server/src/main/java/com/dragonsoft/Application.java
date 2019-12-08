c8b4bad4e0432b4b9d3729bdeaa93696c07d692b
ronin <lwh_ubuntu#163.com>
2019-12-08 20:29:32 +0800

    spring-eurake

--- spring-boot-eureka-server/src/main/java/com/dragonsoft/Application.java
+++ spring-boot-eureka-server/src/main/java/com/dragonsoft/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+//Eurake客户端,接受其他服务注册进来
 @EnableEurekaServer
 @SpringBootApplication
 public class Application {
